21afe2467ed33b26904bdf0474d3c21733d584c5
victor.diez <04.diez.v@gmail.com>
2025-07-09 17:03:15 +0200

    Repo setup

---
+++ notes/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class NoteBook(models.Model):
+    name = models.CharField(max_length=255)
+    users = models.ManyToManyField(User, related_name='notebooks')
+
+    def __str__(self):
+        return self.name
+
+class Attachment(models.Model):
+    file_name = models.CharField(max_length=255)
+    url = models.URLField()
+    def __str__(self):
+        return self.file_name
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+    bloc = models.ForeignKey(NoteBook, on_delete=models.CASCADE, related_name='notes')
+    checked = models.BooleanField(default=False)
+    important = models.BooleanField(default=False)
+    attachments = models.ManyToManyField(Attachment, blank=True, related_name='notes')
+
+
+    class Meta:
+        unique_together = ('bloc', 'title')
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.title} ({self.author.username} - {self.bloc.name})"
+
+
+
+
